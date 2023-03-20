756df0f57fae5c996ad119ec72c4aabe217d84e4
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2023-03-20 16:42:29 +0545

    [ADDED]: added changes where user data is sent when login is valid

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -56,7 +56,8 @@ class AuthController extends Controller
 
         return response()->json([
             'access_token' => $token,
-            'token_type' => 'Bearer'
+            'token_type' => 'Bearer',
+            'user' => $user
         ]);
 
     }
